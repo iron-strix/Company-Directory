f8ace0ba545dd585b70a27b450be4994115b0c4a
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-09-17 18:15:45 -0500

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Montserrat'],
+      serif: ['Roboto Slab'],
+    },
     extend: {},
   },
   plugins: [],
