def1bf6129062b826d6773b239b9abbd6a90ba91
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-09-16 21:47:09 -0500

    added nav layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏦 Fake Company Directory')
+</script>
+<template>
+  <nav class="bg-red-500">
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20 bg-red-900 text-slate-200;
+
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-white;
+        }
+      }
+
+      .menu {
+        @apply flex gap-2;
+        &-item {
+          @apply rounded-md px-4 py-2 hover:bg-rose-300 hover:text-slate-900;
+        }
+        &-login {
+          @apply rounded-md bg-rose-600 px-4 py-2 hover:bg-red-400 hover:text-slate-900;
+        }
+      }
+    }
+  }
+</style>
